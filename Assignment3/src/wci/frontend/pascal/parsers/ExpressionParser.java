14aab24d67ef2bc7eb400b11e783fa769de8f063
Harshpreet Singh <singhharshpreet1993@gmail.com>
2015-09-27 17:12:38 -0700

    add helpful comments
    implement the body of the case statement.

--- Assignment3/src/wci/frontend/pascal/parsers/ExpressionParser.java
+++ Assignment3/src/wci/frontend/pascal/parsers/ExpressionParser.java
@@ -326,6 +326,9 @@ public class ExpressionParser extends StatementParser
 
             // this is where the code goes for building the parse tree for sets
             // add a case for LEFT_BRACKET because '[' signifies that it is a set
+            case LEFT_BRACKET: {
+                //TODO: create parse tree for set expressions here
+            }
 
             default: {
                 errorHandler.flag(token, UNEXPECTED_TOKEN, this);
