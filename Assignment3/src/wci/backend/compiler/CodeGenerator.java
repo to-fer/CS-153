b52f70d7ef2d95b6f08527e4fc7767f6c6ab0868
Udaiveer <uda4000@hotmail.com>
2015-09-30 11:35:48 -0700

    printing the output

--- Assignment3/src/wci/backend/compiler/CodeGenerator.java
+++ Assignment3/src/wci/backend/compiler/CodeGenerator.java
@@ -2,6 +2,7 @@ package wci.backend.compiler;
 
 import wci.backend.*;
 import wci.intermediate.ICode;
+import wci.intermediate.ICodeNode;
 import wci.intermediate.SymTabStack;
 import wci.message.*;
 
@@ -28,9 +29,10 @@ public class CodeGenerator extends Backend
         throws Exception
     {
         long startTime = System.currentTimeMillis();
-        float elapsedTime = (System.currentTimeMillis() - startTime)/1000f;
         int instructionCount = 0;
+        float elapsedTime = (System.currentTimeMillis() - startTime)/1000f;
 
+        
         // Send the compiler summary message.
         sendMessage(new Message(COMPILER_SUMMARY,
                                 new Number[] {instructionCount,
