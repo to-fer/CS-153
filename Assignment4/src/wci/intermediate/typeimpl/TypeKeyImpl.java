ade2ca8dc71b3367936f2fe2691e38c41b3263ea
Udaiveer <uda4000@hotmail.com>
2015-10-09 11:42:19 -0700

    added 2 emum props for TypeKeyImpl for sets

--- Assignment4/src/wci/intermediate/typeimpl/TypeKeyImpl.java
+++ Assignment4/src/wci/intermediate/typeimpl/TypeKeyImpl.java
@@ -22,5 +22,8 @@ public enum TypeKeyImpl implements TypeKey
     ARRAY_INDEX_TYPE, ARRAY_ELEMENT_TYPE, ARRAY_ELEMENT_COUNT,
 
     // Record
-    RECORD_SYMTAB
+    RECORD_SYMTAB,
+    
+    // Set 
+    SET_ELEMENT_TYPE, SET_ELEMENT_COUNT
 }
