7c40effcd958c9a7609acb2c60188f0b857510db
Udaiveer <uda4000@hotmail.com>
2015-11-21 20:48:12 -0800

    TypeSetteVisit is stubed and extends TsParserVisitorAdaptor

--- TestScript/src/wci/intermediate/TypeSetterVisitor.java
+++ TestScript/src/wci/intermediate/TypeSetterVisitor.java
@@ -6,6 +6,74 @@ import wci.intermediate.symtabimpl.*;
 
 public class TypeSetterVisitor extends TSParserVisitorAdapter
 {
-	
+	   private void setType(SimpleNode node)
+	    { /*
+	        int count = node.jjtGetNumChildren();
+	        TypeSpec type = Predefined.integerType;
+	        
+	        for (int i = 0; (i < count) && (type == Predefined.integerType); ++i) {
+	            SimpleNode child = (SimpleNode) node.jjtGetChild(i);
+	            TypeSpec childType = child.getTypeSpec();
+	            
+	            if (childType == Predefined.realType) {
+	                type = Predefined.realType;
+	            }
+	        }
+	        
+	        node.setTypeSpec(type);
+	        */
+	    }
   
+		public Object visit(SimpleNode node, Object data) {
+	        return node.childrenAccept(this, data);
+		}
+
+		public Object visit(ASTprogram node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTCompound_stmt node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTAssignment node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTadd node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTsubtract node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTmultiply node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTdivide node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTidentifier node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTnumber node, Object data) {
+	        return node.childrenAccept(this, data);
+
+		}
+
+		public Object visit(ASTstring node, Object data) {
+	        return node.childrenAccept(this, data);
+		}
 }
