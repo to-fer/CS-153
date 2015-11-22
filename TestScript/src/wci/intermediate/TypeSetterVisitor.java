893c69246880ca6f742b1aaec3f7697cbe62b88f
Udaiveer <uda4000@hotmail.com>
2015-11-21 21:07:52 -0800

    added TypeSetterVisit for setting types to nodes by looking at their first child typeSpec

--- TestScript/src/wci/intermediate/TypeSetterVisitor.java
+++ TestScript/src/wci/intermediate/TypeSetterVisitor.java
@@ -1,79 +1,94 @@
 package wci.intermediate;
 
 import wci.frontend.*;
-import wci.intermediate.*;
 import wci.intermediate.symtabimpl.*;
 
 public class TypeSetterVisitor extends TSParserVisitorAdapter
 {
 	   private void setType(SimpleNode node)
-	    { /*
-	        int count = node.jjtGetNumChildren();
-	        TypeSpec type = Predefined.integerType;
-	        
-	        for (int i = 0; (i < count) && (type == Predefined.integerType); ++i) {
-	            SimpleNode child = (SimpleNode) node.jjtGetChild(i);
-	            TypeSpec childType = child.getTypeSpec();
-	            
-	            if (childType == Predefined.realType) {
-	                type = Predefined.realType;
-	            }
-	        }
-	        
+	    { 
+		   try {
+	        TypeSpec type = Predefined.numberType;
+
+	        SimpleNode child = (SimpleNode) node.jjtGetChild(0);	 
+            TypeSpec childType = child.getTypeSpec();
+
+	        if (childType == Predefined.numberType) {
+                type = Predefined.numberType;
+            } else if(childType == Predefined.booleanType) {
+            	type = Predefined.booleanType;
+            } else if(childType == Predefined.charType) {
+            	type = Predefined.booleanType;
+            } else {
+            	type = Predefined.undefinedType;
+            }
 	        node.setTypeSpec(type);
-	        */
+
+		   } catch (Exception e) {
+			   System.out.println("Node bay not have child in TypeSetterVisitor");
+		        node.setTypeSpec(Predefined.undefinedType);
+		   }
+		   
 	    }
   
 		public Object visit(SimpleNode node, Object data) {
-	        return node.childrenAccept(this, data);
-		}
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;		}
 
 		public Object visit(ASTprogram node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTCompound_stmt node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTAssignment node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTadd node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTsubtract node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTmultiply node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTdivide node, Object data) {
-	        return node.childrenAccept(this, data);
-
+	        Object obj = super.visit(node, data);
+	        setType(node);
+	        return obj;
 		}
 
 		public Object visit(ASTidentifier node, Object data) {
-	        return node.childrenAccept(this, data);
+	        return data;
 
 		}
 
 		public Object visit(ASTnumber node, Object data) {
-	        return node.childrenAccept(this, data);
+	        return data;
 
 		}
 
 		public Object visit(ASTstring node, Object data) {
-	        return node.childrenAccept(this, data);
+	        return data;
 		}
 }
