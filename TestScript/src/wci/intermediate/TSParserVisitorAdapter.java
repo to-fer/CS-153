b6850fdcfc43d9380d6f35a454658cc5c21b9772
Udaiveer <uda4000@hotmail.com>
2015-11-21 20:45:36 -0800

    changed PclParserVisitorAdaptor to TsParserVisitorAdaptor

---
+++ TestScript/src/wci/intermediate/TSParserVisitorAdapter.java
@@ -0,0 +1,85 @@
+package wci.intermediate;
+
+import wci.frontend.ASTAssignment;
+import wci.frontend.ASTCompound_stmt;
+import wci.frontend.ASTadd;
+import wci.frontend.ASTsubtract;
+import wci.frontend.ASTmultiply;
+import wci.frontend.ASTnumber;
+import wci.frontend.ASTprogram;
+import wci.frontend.ASTstring;
+import wci.frontend.ASTdivide;
+import wci.frontend.ASTidentifier;
+import wci.frontend.PclParserVisitor;
+import wci.frontend.SimpleNode;
+import wci.intermediate.symtabimpl.Predefined;
+
+public class TSParserVisitorAdapter implements PclParserVisitor
+{
+
+	@Override
+	public Object visit(SimpleNode node, Object data) {
+        return node.childrenAccept(this, data);
+	}
+
+	@Override
+	public Object visit(ASTprogram node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTCompound_stmt node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTAssignment node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTadd node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTsubtract node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTmultiply node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTdivide node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTidentifier node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTnumber node, Object data) {
+        return node.childrenAccept(this, data);
+
+	}
+
+	@Override
+	public Object visit(ASTstring node, Object data) {
+        return node.childrenAccept(this, data);
+	}
+
+
+}
