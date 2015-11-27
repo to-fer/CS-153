a1841a3b8d52d9e5d807ed8ae423613181c76cc5
Harshpreet Singh <singhharshpreet1993@gmail.com>
2015-11-26 20:37:48 -0800

    changed numberType type code to "F". code generation works now.

--- TestScript/src/wci/backend/compiler/TypeCode.java
+++ TestScript/src/wci/backend/compiler/TypeCode.java
@@ -7,6 +7,7 @@ public class TypeCode {
     private static final String BOOLEAN_TYPECODE = "B";
     private static final String CHAR_TYPECODE = "C";
     private static final String DOUBLE_TYPECODE = "D";
+    private static final String FLOAT_TYPECODE = "F";
 
     public static String typeSpecToTypeCode(TypeSpec spec) {
         if (spec == Predefined.booleanType) {
@@ -16,7 +17,7 @@ public class TypeCode {
             return CHAR_TYPECODE;
         }
         else if (spec == Predefined.numberType) {
-            return DOUBLE_TYPECODE;
+            return FLOAT_TYPECODE;
         }
         else {
             throw new UnsupportedOperationException("That type is not yet implemented in the code generator!");
