f6ac42573e2471510b2eceab6de88a144e957782
Harshpreet Singh <singhharshpreet1993@gmail.com>
2015-12-10 23:33:07 -0800

    added better error messages.

---
+++ TestScript/src/wci/frontend/TypeScriptParseException.java
@@ -0,0 +1,48 @@
+package wci.frontend;
+
+import java.util.ArrayList;
+
+public class TypeScriptParseException extends ParseException{
+	
+	ParseException baseEx;
+	
+	public TypeScriptParseException(ParseException p){
+		super(p.currentToken, p.expectedTokenSequences, p.tokenImage);
+	}
+	
+	public ArrayList<String> getExpectedTokens() {
+		ArrayList<String> result = new ArrayList<String>();
+		int maxSize = 0;
+		for (int i = 0; i < expectedTokenSequences.length; i++) {
+			if (maxSize < expectedTokenSequences[i].length) {
+		        maxSize = expectedTokenSequences[i].length;
+		      }
+		      for (int j = 0; j < expectedTokenSequences[i].length; j++) {
+		        result.add(tokenImage[expectedTokenSequences[i][j]]);
+		      }
+		}
+		return result;
+	}
+	
+	@Override
+	public String getMessage() {
+		String message = "";
+		message += "-----------ERROR----------- \n";
+		message += "LOCATION: line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn+'\n';
+		message += "ENCOUNTERED: "+currentToken.image+'\n';
+		message += "EXPECTED ONE OF THE FOLLOWING: \n";
+		if(getExpectedTokens().size() == 1) {
+			message += "      "+getExpectedTokens().get(0) + '\n';
+		}
+		else if(getExpectedTokens().size() > 1) {
+			for(String exp: getExpectedTokens()) {
+				message += "      "+exp + '\n';
+			}
+		}
+		
+		return message;
+		
+	}
+
+
+}
